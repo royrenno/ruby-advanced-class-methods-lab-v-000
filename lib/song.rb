54912acdc0ec9c5b83595dc377dd9759e44132f0
roy renno <royrenno@yahoo.com>
2019-04-06 21:44:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,38 +14,45 @@ class Song
     song.save
     song
   end
+  
   def self.new_by_name(song_name)
     song = self.new
     song.name = song_name
     song
   end
+  
   def self.create_by_name(song_name)
     song = self.create
     song.name = song_name
     song
   end
+  
   def self.find_by_name(song_name)
     self.all.detect{|s|s.name == song_name}
   end
+  
   def self.find_or_create_by_name(song_name)
     self.find_by_name(song_name) || self.create_by_name(song_name)
   end
+  
 def self.alphabetical
   self.all.sort_by{|s| s.name}
 end
+
 def.self.new_from_filename(filename)
   parts = filename.split(" - ")
   artist.name = parts[0]
-  song.name = parts[1].gsub(".mp3","")
+  song.name = parts[1].gsub(".mp3", "")
+  
   song = self.new
   song.name = song.name
   song.artist_name = artist_name
   song
 end
 def self.create_from_filename(filename)
-  parts = filename.split("-")
+  parts = filename.split(" - ")
   artist.name = parts([0]
-  song.name = parts[1].gsub(".mp3","")
+  song.name = parts[1].gsub(".mp3", "")
   song = self.create
   song.name = song.name
   song.artist_name = artist_name
