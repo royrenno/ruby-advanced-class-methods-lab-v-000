2a3cbe4b7f0c3a8ea9702a26d055b0f0cb3c62f1
roy renno <royrenno@yahoo.com>
2019-04-06 21:52:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,8 +53,8 @@ end
 
 def self.create_from_filename(filename)
   parts = filename.split(" - ")
-  artist.name = parts[0]
-  song.name = parts[1].gsub(".mp3", "")
+  artist_name = parts[0]
+  song_name = parts[1].gsub(".mp3", "")
   song = self.create
   song.name = song_name
   song.artist_name = artist_name
